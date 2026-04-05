2b92db1c69ba5f6aeec6a12446c3c499145272d4
Angel Rios <angel.rios.dev@gmail.com>
2026-04-04 20:53:18 -0600

    NO AGREGAR EL .env a prod, como son contenedores locales no importa, ya que se borran

--- app/bd/postgres/connection.py
+++ app/bd/postgres/connection.py
@@ -1,1 +1,15 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.core.config import settings
 
+engine = create_engine(settings.POSTGRES_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autflush=False, bind=engine)
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
